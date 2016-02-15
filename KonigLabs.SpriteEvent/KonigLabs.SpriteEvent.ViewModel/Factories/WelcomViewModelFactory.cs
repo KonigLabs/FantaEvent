e7e10fbae98a19b87cbdb72ba5a736b82d54109d
Petrachkov Artem <petrachkovartem@live.ru>
2016-02-15 22:31:24 +0200

    Initial

---
+++ KonigLabs.SpriteEvent/KonigLabs.SpriteEvent.ViewModel/Factories/WelcomViewModelFactory.cs
@@ -0,0 +1,18 @@
+﻿using KonigLabs.SpriteEvent.CommonViewModel.Factories;
+using KonigLabs.SpriteEvent.ViewModel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonigLabs.SpriteEvent.ViewModel.Factories
+{
+    public class WelcomViewModelFactory : ViewModelBaseFactory<WelcomViewModel>
+    {
+        protected override WelcomViewModel GetViewModel(object param)
+        {
+            return new WelcomViewModel();
+        }
+    }
+}
---
+++ KonigLabs.SpriteEvent/KonigLabs.SpriteEvent.ViewModel/Ninject/NinjectMainModule.cs
@@ -0,0 +1,45 @@
+﻿
+using KonigLabs.SpriteEvent.CommonViewModel.Factories;
+using KonigLabs.SpriteEvent.CommonViewModel.Messenger;
+using KonigLabs.SpriteEvent.CommonViewModel.Navigator;
+using KonigLabs.SpriteEvent.ViewModel.Factories;
+using KonigLabs.SpriteEvent.ViewModel.ViewModels;
+using Ninject;
+using Ninject.Modules;
+using System.Collections.Generic;
+
+namespace KonigLabs.SpriteEvent.ViewModel.Ninject
+{
+    public class NinjectMainModule: NinjectModule
+    {
+        public override void Load()
+        {
+
+            Bind<IMessenger>().To<MvvmLightMessenger>().InSingletonScope();
+            Bind<ViewModelStorage>().ToSelf().InSingletonScope();
+            Bind<MainViewModel>().ToSelf();
+            Bind<TakePhotoViewModelFactory>().ToSelf();
+            Bind<WelcomViewModelFactory>().ToSelf();
+            Bind<IViewModelNavigator>().To<ViewModelNavigator>().WithConstructorArgument(typeof(IChildrenViewModelsFactory),
+                    x =>
+                    {
+                        var children = new List<IViewModelFactory>
+                        {
+                            x.Kernel.Get<WelcomViewModelFactory>()
+                        };
+
+                        return new ChildrenViewModelsFactory(children);
+                    });
+            Bind<IViewModelNavigator>().To<ViewModelNavigator>().WhenInjectedExactlyInto<MainViewModel>().WithConstructorArgument(typeof(IChildrenViewModelsFactory), x =>
+            {
+                var children = new List<IViewModelFactory>
+                {
+                    x.Kernel.Get<WelcomViewModelFactory>(),
+                    
+                };
+
+                return new ChildrenViewModelsFactory(children);
+            });
+        }
+    }
+}
